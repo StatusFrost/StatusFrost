530fced74203137a3e6c08fa3679c633e889f6b7
Dylan Katz <dylanishappy1@gmail.com>
2016-12-25 14:30:33 -0700

    Added percentage display
    Thanks to David Frame for the base code.

--- src/browser_action/script.js
+++ src/browser_action/script.js
@@ -118,7 +118,26 @@ function renderDonutChart(keys, values, ctx, chartName) {
     };
     var doughnutChart = new Chart(ctx, {
         type: 'doughnut',
-        data: data
+        data: data,
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        var allData = data.datasets[tooltipItem.datasetIndex].data;
+                        var tooltipLabel = data.labels[tooltipItem.index];
+                        var tooltipData = allData[tooltipItem.index];
+                        var total = 0;
+                        for (var i in allData) {
+                            if(allData[i]) {
+                                total += allData[i];
+                            }
+                        }
+                        var tooltipPercentage = Math.round((tooltipData / total) * 100);
+                        return tooltipLabel + ': ' + tooltipData + ' (' + tooltipPercentage + '%)';
+                    }
+                }
+            }
+        }
     });
 }
 
